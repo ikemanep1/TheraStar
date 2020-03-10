2e322859671073fa111b06819343af2ca2872364
ikemanep1 <ike.esquivelpilloud@gmail.com>
2020-03-10 16:16:57 -0700

    clear state lists

--- src/App.js
+++ src/App.js
@@ -25,104 +25,26 @@ class App extends React.Component {
     this.state = {
       items: [],
       isLoaded: false,
-      masterMhpList: [{
-        name: "Valarie Tobin, CNS",
-        occupation: "CNS-Child & Adolescent Psychiatric & Mental Health ( Primary Specialty )",
-        address: "1700 W. Van Buren St. Suite 5827-B Chicago, IL 60612",
-        insurance: "Can be found on site",
-        accepting: "Is currently accepting patients",
-        email: "unavailable",
-        phone: "(312) 563-1579",
-        bio: "Valarie specializes in the mental health of children.",
-        link: "https://doctors.rush.edu/details/17816/valerie-tobin-cns-child__adolescent_psychiatric__mental_health-chicago"
-      }],
-      masterReviewList: [
-        {
-          name: "steve",
-          subject: "Marvin",
-          rating: "5",
-          content: "he is the best!"
-        },
-        {
-          name: "steve",
-          subject: "Marvin",
-          rating: "5",
-          content: "he is the best!"
-        },
-        {
-          name: "steve",
-          subject: "Marvin",
-          rating: "5",
-          content: "he is the best!"
-        },
-        {
-          name: "steve",
-          subject: "Marvin",
-          rating: "5",
-          content: "he is the best!"
-        },
-        {
-          name: "steve",
-          subject: "Marvin",
-          rating: "5",
-          content: "he is the best!"
-        },
-        {
-          name: "steve",
-          subject: "Marvin",
-          rating: "5",
-          content: "he is the best!"
-        },
-      ],
-      masterArticleList: [
-        {
-          name: "Stress management",
-          link: "https://www.helpguide.org/articles/stress/stress-management.htm",
-          description: "This article covers the importance of being able to manage stress, and provides tips on how to do it."
-        },
-        {
-          name: "How to handle stress at work",
-          link: "https://www.health.harvard.edu/blog/how-to-handle-stress-at-work-2019041716436",
-          description: "This article published by Harvard Medical School covers stress in the workplace, how it affects the body, and provides tips on how to take care of yourself."
-        },
-        {
-          name: "Finding a provider",
-          link: "https://www.healthcare.gov/blog/finding-a-provider/",
-          description: "This article provides tips on finding a mental health practitioner."
-        },
-        {
-          name: "Supporting someone with a mental health condition",
-          link: "https://www.beyondblue.org.au/the-facts/supporting-someone/supporting-someone-with-depression-or-anxiety",
-          description: "This article details the importance of being supportive of those who have mental health conditions."
-        },
-        {
-          name: "Six Ways to Stand Behind Your LGBT Students",
-          link: "https://www.tolerance.org/magazine/six-ways-to-stand-behind-your-lgbt-students",
-          description: "This article covers the importance of being inclusive and supportive of LGBT communities in the classroom."
-        },
-        {
-          name: "This Way Up",
-          link: "https://thiswayup.org.au/how-do-you-feel/worried/",
-          description: "This article talks about anxiety, and provides tips on how to cope with it."
-        },
-      ],
+      masterMhpList: [],
+      masterReviewList: [],
+      masterArticleList: [],
     }
     this.handleAddingNewArticleToList = this.handleAddingNewArticleToList.bind(this);
     this.handleAddingNewReviewToList = this.handleAddingNewReviewToList.bind(this);
   };
 
-  componentDidMount() {
-  fetch('https://obscure-tor-14240.herokuapp.com/articles')
-    .then(res => res.json())
-    .then(json => {
-      this.setState({
-        isLoaded: true,
-        items: json,
-      })
-      console.log(json)
-    });
-    console.log(this.state.items)
-}
+//   componentDidMount() {
+//   fetch('https://obscure-tor-14240.herokuapp.com/articles')
+//     .then(res => res.json())
+//     .then(json => {
+//       this.setState({
+//         isLoaded: true,
+//         items: json,
+//       })
+//       console.log(json)
+//     });
+//     console.log(this.state.items)
+// }
 
   handleAddingNewArticleToList(newArticle) {
     let newMasterArticleList = this.state.masterArticleList.slice();
@@ -137,10 +59,10 @@ class App extends React.Component {
   }
   render() {
 
-      const { isLoaded, items } = this.state;
-      if (!isLoaded) {
-        return <div>Loading</div>;
-      }
+      // const { isLoaded, items } = this.state;
+      // if (!isLoaded) {
+      //   return <div>Loading</div>;
+      // }
     return (
       <div className="App">
 
